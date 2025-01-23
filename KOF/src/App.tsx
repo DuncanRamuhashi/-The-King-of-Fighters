19370ff0a1f498d9b66ceec04715ea6ebb399134
Duncan Ramuhashi <duncanramohashi@gmail.com>
2025-01-23 14:27:41 +0200

    start

---
+++ KOF/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <>
+
+      <p className="text-4xl bg-red-500 py-100">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
