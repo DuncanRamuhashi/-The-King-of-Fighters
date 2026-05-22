8ea8ef4c17988dccd96732b6629a5aa9a226caa4
Duncan Ramuhashi <duncanramohashi@gmail.com>
2026-05-22 07:13:36 +0200

    de

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,17 @@ import connectDB from './config/db.js';
 import stageRoutes from './routes/stageRoutes.js';
 import playerRouters from './routes/playerRoutes.js';
 import cors from "cors";
+
 dotenv.config();
 
 const app = express();
+
 connectDB();
-const PORT = process.env.PORT || 4000;
 
-// Use express.json to parse incoming JSON requests, with an increased limit
-app.use(express.json({ limit: '10mb' }));  // limit issues again
-
+app.use(express.json({ limit: '10mb' }));
 app.use(cors());
+
 app.use("/api/stages", stageRoutes);
 app.use("/api/players", playerRouters);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
